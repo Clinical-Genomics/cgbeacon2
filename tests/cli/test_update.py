2cd738d23021521a42f8c057e3c17494a2c9dcb2
Chiara Rasi <rasi.chiara@gmail.com>
2022-03-31 14:06:10 +0200

    Avoid using deprecated match_querystring in mock response

--- tests/cli/test_update.py
+++ tests/cli/test_update.py
@@ -4,21 +4,7 @@ from cgbeacon2.cli.commands import cli
 from cgbeacon2.utils.ensembl_biomart import BIOMART_38
 
 # Example of query runned on the EnsemblBiomartClient
-XML_QUERY = """<?xml version="1.0" encoding="UTF-8"?>
-	<!DOCTYPE Query>
-	<Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" completionStamp = "1">
-
-		<Dataset name = "hsapiens_gene_ensembl" interface = "default" >
-			<Filter name = "chromosome_name" value = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y,MT"/>
-			<Attribute name = "ensembl_gene_id" />
-			<Attribute name = "hgnc_id" />
-			<Attribute name = "hgnc_symbol" />
-			<Attribute name = "chromosome_name" />
-			<Attribute name = "start_position" />
-			<Attribute name = "end_position" />
-		</Dataset>
-	</Query>
-	"""
+XML_QUERY = """%3C?xml%20version=%221.0%22%20encoding=%22UTF-8%22?%3E%0A%3C!DOCTYPE%20Query%3E%0A%3CQuery%20%20virtualSchemaName%20=%20%22default%22%20formatter%20=%20%22TSV%22%20header%20=%20%220%22%20uniqueRows%20=%20%220%22%20count%20=%20%22%22%20datasetConfigVersion%20=%20%220.6%22%20completionStamp%20=%20%221%22%3E%0A%0A%09%3CDataset%20name%20=%20%22hsapiens_gene_ensembl%22%20interface%20=%20%22default%22%20%3E%0A%09%09%3CFilter%20name%20=%20%22chromosome_name%22%20value%20=%20%221,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y,MT%22/%3E%0A%09%09%3CAttribute%20name%20=%20%22ensembl_gene_id%22%20/%3E%0A%09%09%3CAttribute%20name%20=%20%22hgnc_id%22%20/%3E%0A%09%09%3CAttribute%20name%20=%20%22hgnc_symbol%22%20/%3E%0A%09%09%3CAttribute%20name%20=%20%22chromosome_name%22%20/%3E%0A%09%09%3CAttribute%20name%20=%20%22start_position%22%20/%3E%0A%09%09%3CAttribute%20name%20=%20%22end_position%22%20/%3E%0A%09%3C/Dataset%3E%0A%3C/Query%3E"""
 
 
 @responses.activate
@@ -37,7 +23,7 @@ def test_update_genes_build_38(mock_app, database):
         b"ENSG00000232218\t\t\t22\t32386668\t32386868\n"
         b"[success]"
     )
-    responses.add(responses.GET, url, body=response, status=200, match_querystring=False)
+    responses.add(responses.GET, url, body=response, status=200)
 
     # test add a dataset_obj using the app cli
     runner = mock_app.test_cli_runner()
